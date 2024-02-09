f597666fe003b3bf735af27b793767ca3c588372
DanielleZror <daniellezror@gmail.com>
2024-02-08 21:08:28 -0600

    Profile pic

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,9 @@ export default function About() {
         <section className="bg-black-400">
             <h2 className="mb-10">About Me</h2>
             <h4>Hello, I'm Danielle Zror</h4>
+            <div className="flex justify-center transition-height duration-500 ease-in-out md:h-0 max-md:h-64">
+                <img src="Profile.png" alt="Profile" />
+            </div>
             <h5>
                 I'm an experienced software engineer with a strong product mindset, skilled in Python, frontend frameworks, SQL, and OOP principles. Excellent problem-solving, creative design solutions, and architectural capabilities.
                 Highly motivated, technology-driven, self-learner, independent, and a team player.
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,18 +3,13 @@ import Typewriter from "./Typewriter"
 
 export default function Home() {
     return (
-        <section className="min-h-screen flex flex-col">
-            <div className="text-left flex mt-6 ">
-                <div className="w-2/3 pt-20 max-sm:pt-12">
-                    <h3 className="mb-4">Hello,</h3>
-                    <Typewriter texts={["Danielle Zror", "a Software Enginee", "a Troubleshooting Pro", "a Dog Mom"]} />
-                    <h5 className="mt-12">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at sapien vitae nisl aliquam fringilla. Etiam tempus augue vitae eleifend lacinia. Duis at leo dignissim, iaculis risus vitae, finibus tellus. Cras ullamcorper ut justo at tempor. Nulla convallis est et orci faucibus semper. Nam pulvinar hendrerit est, blandit pretium felis.
-                    </h5>
-                </div>
-                <div className="w-1/3 self-center">
-                    <img src="Github.png" alt="Profile" />
-                </div>
+        <section className="min-h-screen flex flex-col bg-no-repeat bg-right-bottom md:bg-profile-pic max-md:bg-none">
+            <div className="text-left mt-6 pt-20 max-sm:pt-12">
+                <h3 className="mb-4">Hello,</h3>
+                <Typewriter texts={["Danielle Zror", "a Software Enginee", "a Troubleshooting Pro", "a Dog Mom"]} />
+                <h5 className="mt-12">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at sapien vitae nisl aliquam fringilla. Etiam tempus augue vitae eleifend lacinia. Duis at leo dignissim, iaculis risus vitae, finibus tellus. Cras ullamcorper ut justo at tempor. Nulla convallis est et orci faucibus semper. Nam pulvinar hendrerit est, blandit pretium felis.
+                </h5>
             </div>
             <ContactDetails />
         </section>
