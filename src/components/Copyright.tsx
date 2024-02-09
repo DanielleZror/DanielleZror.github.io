a1ff81cfd24a7d9dedb3c2723840a031a1a418e6
DanielleZror <daniellezror@gmail.com>
2024-02-09 13:09:35 -0600

    text selection color

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,13 +3,9 @@ import { LuCopyright } from "react-icons/lu"
 
 export default function Copyright() {
     return (
-        <div className="bg-black flex flex-row m-8 items-center justify-center">
-            <h5 className="mr-2 ">
-                Copyright
-            </h5>
-            <LuCopyright size={22} color="#E9A5C5" />
-            <h5 className="ml-2 ">
-                2024 Danielle Zror. All Rights Resereved.
+        <div className="bg-black m-8">
+            <h5 className="flex flex-row items-center justify-center">
+                Copyright <LuCopyright className="ml-2 mr-2" size={22} color="#E9A5C5" /> 2024 Danielle Zror. All Rights Resereved.
             </h5>
         </div>
     )
