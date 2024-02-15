4a8bcbe134e0e01b72801d1c13fca77be42737a9
DanielleZror <daniellezror@gmail.com>
2024-02-15 12:24:44 -0600

    Fix: typo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <section className="min-h-[90vh] max-md:min-h-[80vh] flex flex-col bg-no-repeat bg-right-bottom md:bg-profile-pic dark:md:bg-profile-pic-dark max-md:bg-none">
             <div className="text-left mt-6 pt-20 max-sm:pt-12">
                 <h3 className="mb-4">Hello,</h3>
-                <Typewriter texts={["Danielle Zror", "a Software Enginee", "a Troubleshooting Pro", "a Dog Mom"]} />
+                <Typewriter texts={["Danielle Zror", "a Software Engineer", "a Troubleshooting Pro", "a Dog Mom"]} />
                 <h5 className="mt-12 md:w-4/5 w-full">
                     Hey there! I am Danielle, I juggle code and creativity, I brew ideas like a barista crafts coffee, and I turn complex problems into elegant solutions! When I'm not busy turning coffee into code and debugging with my trusty rubber duck, you'll likely find me hitting the gym or enjoying quality time with my furry dog Lola.
                 </h5>
